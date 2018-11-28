be0c090021e9da3a86a6ce3608eccd5866fdbed1
Justin Dalton <jdalton15@gmail.com>
2018-11-28 14:53:55 -0500

    parsing entity rows from file into objects

---
+++ src/utils/__tests__/parse-entities.test.js
@@ -0,0 +1,16 @@
+const parseEntities = require('../parse-entities');
+
+const sampleInput = [
+  'Driver Dan',
+  'Driver Alex',
+  'Driver Bob',
+  'Trip Dan 07:15 07:45 17.3',
+  'Trip Dan 06:12 06:32 21.8',
+  'Trip Alex 12:01 13:16 42.0',
+];
+
+test('parses the input and returns drivers with trips', () => {
+  const result = parseEntities(sampleInput);
+  expect(result.length).toEqual(3);
+  expect(result[0].getTrips().length).toEqual(2);
+});
---
+++ src/utils/parse-entities.js
@@ -0,0 +1,27 @@
+const createDriver = require('../factories/create-driver');
+const createTrip = require('../factories/create-trip');
+
+module.exports = function parseEntities(input) {
+  const drivers = {};
+  const trips = [];
+
+  input.forEach((entity) => {
+    const [type, name] = entity.split(' ');
+
+    if (type === 'Driver') {
+      if (!drivers[name]) {
+        drivers[name] = createDriver({ name });
+      }
+    } else if (type === 'Trip') {
+      const newTrip = createTrip();
+      newTrip.parseTripString(entity);
+      trips.push(newTrip);
+    }
+  });
+
+  trips.forEach((trip) => {
+    drivers[trip.get().driver].addTrip(trip);
+  });
+
+  return Object.values(drivers);
+};
