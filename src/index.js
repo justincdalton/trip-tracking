18fb7c35ee5a5e03df43822d11752f7a2231ef32
Justin Dalton <jdalton15@gmail.com>
2018-12-19 13:27:50 -0500

    adding koa with basic route and render capabilities

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
+const Koa = require('koa');
+const logger = require('koa-logger');
+const send = require('koa-send');
+const Router = require('koa-router');
+const render = require('./middleware/render');
+
 const parseFile = require('./utils/parse-file');
 const parseEntities = require('./utils/parse-entities');
 const exportReport = require('./utils/export-report');
 
-const main = async () => {
-  const entities = await parseFile('src/input.txt');
-  const drivers = parseEntities(entities);
-  exportReport.generate('src/report.txt', drivers);
+const app = new Koa();
+const router = new Router();
+
+app.use(logger());
+app.use(render);
+
+const home = async (ctx) => {
+  await ctx.render('home');
 };
 
-main();
+const report = async (ctx) => {
+  const entities = await parseFile('src/input.txt');
+  const drivers = parseEntities(entities);
+  await exportReport.generate('src/report.txt', drivers);
+  await send(ctx, 'src/report.txt');
+};
+
+router.get('/', home).get('/report', report);
+
+app.use(router.routes());
+
+app.listen(3000);
+console.log('listening on port 3000');
